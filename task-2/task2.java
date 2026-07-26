22d8b21e7f3692f6bf8af3fcd0c93564a70ae872
vtu29703-boop <vtu29703@veltech.edu.in>
2026-07-26 08:38:55 +0530

    Implement Nth Fibonacci number calculation
    Fix syntax errors in Fibonacci calculation.

---
+++ task-2/task2.java
@@ -0,0 +1,32 @@
+import java.util. Scanner;
+
+public class NthFibonacci { 
+  public static void main(String[] args) { 
+    Scanner sc = new Scanner (System.in);
+
+System.out.print("Enter n: ");
+    int n = sc.nextInt();
+
+int a= 0, b= 1, c= 0;
+
+if (n == 0) {
+
+System.out.println("Nth Fibonacci number = 0");
+} else if (n == 1) {
+  
+System.out.println("Nth Fibonacci number = 1");
+
+} else {
+
+for (int i = 2: i <= n: i++) {
+     c=a+b;
+     a=b;
+     b=c;
+
+}
+
+System.out.println("Nth Fibonacci number = " + c);
+}
+sc.close();
+    }
+}
