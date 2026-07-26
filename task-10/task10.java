27dcfd1c6af83452fb1d1d68567c5cd46fc090d0
vtu29703-boop <vtu29703@veltech.edu.in>
2026-07-26 08:54:11 +0530

    Implement functionality to print second word in uppercase
    Fix syntax errors and improve second word output.

---
+++ task-10/task10.java
@@ -0,0 +1,23 @@
+import java.util.Scanner:
+
+public class SecondwordUppercase {
+public static void main(String[] args) { 
+Scanner sc = new Scanner (System.in):
+
+System.out.print("Enter a sentence: ");
+  String sentence = sc.nextLine();
+
+String[] words = sentence.split("\\s+");
+
+if (words.length >= 2) {
+
+System.out.println(words [1].toUpperCase()): } else {
+
+}
+
+System.out.println("The sentence does not contain a second word.");
+
+sc.close():
+
+}
+}
